615d6b28f61afd173f58dade1344a290150cdc12
woo <albertwoo_slaveoftime@hotmail.com>
2022-01-05 07:29:35 +0800

    Upgrade GrpcDemo to net6.0

--- GrpcDemo/GrpcDemo.Server/Program.fs
+++ GrpcDemo/GrpcDemo.Server/Program.fs
@@ -1,18 +1,47 @@
-namespace GrpcDemo.Server
-
-open Microsoft.AspNetCore.Hosting
+open System
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open GrpcDemo.Protos.Greeter
 
 
-module Program =
-    let createHostBuilder args =
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.UseStartup<Startup>() |> ignore
-            )
+type GreeterService() =
+    inherit Greeter.GreeterBase()
 
-    [<EntryPoint>]
-    let main args =
-        createHostBuilder(args).Build().Run()
+    override _.SayHello(req, ctx) =
+        task {
+            do! Task.Delay 1000
+            return HelloReply(Message = "Hello from grpc")
+        }
 
-        0 // Exit code
+
+let builder = WebApplication.CreateBuilder(Environment.GetCommandLineArgs())
+
+builder.Services.AddGrpc() |> ignore
+
+builder.Services.AddCors(fun options ->
+    options.AddPolicy(
+        "AllowAll",
+        fun builder ->
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding")
+            |> ignore
+    )
+)
+|> ignore
+
+
+let app = builder.Build()
+
+app.UseCors() |> ignore
+app.UseGrpcWeb() |> ignore
+
+app.MapGrpcService<GreeterService>().EnableGrpcWeb().RequireCors("AllowAll") |> ignore
+app.MapGet("/", (fun context -> context.Response.WriteAsync("Hello World!"))) |> ignore
+
+app.Run()
