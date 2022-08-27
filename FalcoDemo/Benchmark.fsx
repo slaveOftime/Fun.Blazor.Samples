e0d257e5552152afb19b2b836e5e13a6a2967039
binwen <albertwoo_slaveoftime@hotmail.com>
2022-08-27 12:08:26 +0800

    Add falco demo

---
+++ FalcoDemo/Benchmark.fsx
@@ -0,0 +1,37 @@
+#r "nuget: NBomber"
+
+open System.Net.Http
+open FSharp.Control.Tasks.NonAffine
+open NBomber.Contracts
+open NBomber.FSharp
+
+
+let httpClient = new HttpClient()
+
+
+Scenario.create "Fun.Blazor" [
+    Step.create (
+        "fetch fun-form",
+        fun _ -> task {
+            let! response = httpClient.GetAsync("https://localhost:62162/fun-form")
+            return if response.IsSuccessStatusCode then Response.ok () else Response.fail ()
+        }
+    )
+]
+|> NBomberRunner.registerScenario
+|> NBomberRunner.run
+|> printfn "%A"
+
+
+Scenario.create "Falco.Marup" [
+    Step.create (
+        "fetch falco-form",
+        fun _ -> task {
+            let! response = httpClient.GetAsync("https://localhost:62162/falco-form")
+            return if response.IsSuccessStatusCode then Response.ok () else Response.fail ()
+        }
+    )
+]
+|> NBomberRunner.registerScenario
+|> NBomberRunner.run
+|> printfn "%A"
---
+++ FalcoDemo/Program.fs
@@ -0,0 +1,63 @@
+module FalcoDemo.Program
+
+open System
+open Falco
+open Falco.Markup
+open Falco.Routing
+open Falco.HostBuilder
+open Fun.Blazor
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+
+
+module Response =
+
+    let ofFunHtml node : HttpHandler = fun ctx -> ctx.WriteFunDom node
+
+
+// ------------
+// Exception Handler
+// ------------
+let exceptionHandler: HttpHandler = Response.withStatusCode 500 >> Response.ofPlainText "Server error"
+
+
+
+let falcoForm =
+    Templates.html5 "en" [] [
+        Elem.body [] [
+            Elem.form [ Attr.method "post" ] [
+                Elem.input [ Attr.name "Person.first_name" ]
+                Elem.input [ Attr.name "Person.last_name" ]
+                Elem.br []
+                Elem.input [ Attr.type' "Submit" ]
+            ]
+        ]
+    ]
+
+let funForm = fragment {
+    doctype "html"
+    html' {
+        lang "en"
+        body {
+            form {
+                method "post"
+                input { name "Person.first_name" }
+                input { name "Person.last_name" }
+                br
+                input { type' "Submit" }
+            }
+        }
+    }
+}
+
+webHost (Environment.GetCommandLineArgs()) {
+    add_service (fun services ->
+        services.AddControllersWithViews() |> ignore
+        services.AddFunBlazorServer()
+    )
+    endpoints [
+        get "/ping" (Response.ofPlainText "pong")
+        get "/fun-form" (Response.ofFunHtml funForm)
+        get "/falco-form" (Response.ofHtml falcoForm)
+    ]
+}
