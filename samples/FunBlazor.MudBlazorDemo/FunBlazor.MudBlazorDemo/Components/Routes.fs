82c779fff98b7b489c9d5eb8014e7464370f4799
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2024-02-17 17:09:33 +0800

    INIT by template

---
+++ samples/FunBlazor.MudBlazorDemo/FunBlazor.MudBlazorDemo/Components/Routes.fs
@@ -0,0 +1,16 @@
+namespace FunBlazor.MudBlazorDemo.Components
+
+open System.Reflection
+open Fun.Blazor
+open FunBlazor.MudBlazorDemo.Components.Layout
+
+type Routes() =
+    inherit FunComponent()
+
+    override _.Render() = Router'() {
+        AppAssembly(Assembly.GetExecutingAssembly())
+        Found(fun routeData -> RouteView'() {
+            RouteData routeData
+            DefaultLayout typeof<MainLayout>
+        })
+    }
