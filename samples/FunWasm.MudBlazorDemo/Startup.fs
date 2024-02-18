858be968399960119f30065eb8f6a699a2e97834
binwen <albertwoo_slaveoftime@hotmail.com>
2024-02-18 10:25:43 +0800

    INIT FunWasm.MudBlazorDemo

---
+++ samples/FunWasm.MudBlazorDemo/Startup.fs
@@ -0,0 +1,19 @@
+﻿#nowarn "0020"
+
+open System
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open Fun.Blazor
+open FunWasm.MudBlazorDemo
+
+let builder = WebAssemblyHostBuilder.CreateDefault(Environment.GetCommandLineArgs())
+
+#if DEBUG
+builder.AddFunBlazor("#app", app) //html.hotReloadComp(app, "FunWasm.MudBlazorDemo.App.app"))
+#else
+builder.AddFunBlazor("#app", app)
+#endif
+
+builder.Services.AddFunBlazorWasm()
+
+builder.Build().RunAsync()
