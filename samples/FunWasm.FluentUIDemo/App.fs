fc79ac0ad39641119a76a9f802375a15ce6870f1
binwen <albertwoo_slaveoftime@hotmail.com>
2024-02-19 13:24:05 +0800

    INIT FunWasm.FluentUIDemo

---
+++ samples/FunWasm.FluentUIDemo/App.fs
@@ -0,0 +1,22 @@
+// hot-reload
+// hot-reload is the flag to let cli know this file should be included
+// It has dependency requirement: the root is the app which is used in the Startup.fs
+// All other files which want have hot reload, need to drill down to that file, and all the middle file should also add the '// hot-reload' flag at the top of taht file
+[<AutoOpen>]
+module FunWasm.FluentUIDemo.App
+
+open FSharp.Data.Adaptive
+open Fun.Blazor
+
+let app =
+    adaptiview () {
+        let! count, setCount = cval(1).WithSetter()
+
+        div.create [|
+            div { $"Here is the count {count}" }
+            button {
+                on.click (fun _ -> setCount (count + 2))
+                "Increase by 2"
+            }
+        |]
+    }
---
+++ samples/FunWasm.FluentUIDemo/Startup.fs
@@ -0,0 +1,19 @@
+﻿#nowarn "0020"
+
+open System
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open Fun.Blazor
+open FunWasm.FluentUIDemo
+
+let builder = WebAssemblyHostBuilder.CreateDefault(Environment.GetCommandLineArgs())
+
+#if DEBUG
+builder.AddFunBlazor("#app", html.hotReloadComp(app, "FunWasm.FluentUIDemo.App.app"))
+#else
+builder.AddFunBlazor("#app", app)
+#endif
+
+builder.Services.AddFunBlazorWasm()
+
+builder.Build().RunAsync()
