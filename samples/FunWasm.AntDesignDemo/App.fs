399fda52caba06a6eb523dc3b5777d1ce6353862
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2024-02-19 08:29:18 +0800

    Add AntDesign

--- samples/FunWasm.AntDesignDemo/App.fs
+++ samples/FunWasm.AntDesignDemo/App.fs
@@ -5,18 +5,127 @@
 [<AutoOpen>]
 module FunWasm.AntDesignDemo.App
 
-open FSharp.Data.Adaptive
+open AntDesign
 open Fun.Blazor
+open Fun.Blazor.Router
+open Microsoft.AspNetCore.Components.Web
 
-let app =
-    adaptiview () {
-        let! count, setCount = cval(1).WithSetter()
+type IShareStore with
 
-        div.create [|
-            div { $"Here is the count {count}" }
-            button {
-                on.click (fun _ -> setCount (count + 2))
-                "Increase by 2"
+    member store.Count = store.CreateCVal(nameof store.Count, 0)
+    member store.IsMenuOpen = store.CreateCVal(nameof store.IsMenuOpen, false)
+
+let homePage =
+    html.fragment [|
+        SectionContent'() {
+            SectionName "Title"
+            "Home"
+        }
+        Title'() {
+            Level 1
+            Type TextElementType.Danger
+            "Hi from FunBlazor"
+        }
+    |]
+
+let counterPage =
+    html.injectWithNoKey (fun (store: IShareStore, snackbar: NotificationService) ->
+        html.fragment [|
+            SectionContent'() {
+                SectionName "Title"
+                "Counter"
+            }
+            adaptiview () {
+                let! count = store.Count
+                div {
+                    "Here is the count: "
+                    count
+                }
+            }
+            Button'() {
+                Type ButtonType.Primary
+                OnClick(fun _ ->
+                    task {
+                        store.Count.Publish((+) 1)
+                        let! _ = snackbar.Open(NotificationConfig(Message = $"Count = {store.Count.Value}"))
+                        ()
+                    }
+                )
+                "Increase by 1"
             }
         |]
+    )
+
+
+let appbar =
+    html.injectWithNoKey (fun (store: IShareStore) ->
+        html.fragment [|
+            Header'.create [|
+                Button'() {
+                    Shape ButtonShape.Circle
+                    Icon IconType.Outline.MenuFold
+                    OnClick(fun _ -> store.IsMenuOpen.Publish(not))
+                }
+                span {
+                    style { color "white" }
+                    SectionOutlet'() { SectionName "Title" }
+                }
+            |]
+        |]
+    )
+
+let navmenus =
+    Sider'() {
+        Width 200
+        Menu'() {
+            Mode MenuMode.Inline
+            style {
+                height "100%"
+                borderRight "0"
+            }
+            childContent [|
+                MenuItem'() {
+                    a {
+                        href "/"
+                        "Home"
+                    }
+                }
+                MenuItem'() {
+                    a {
+                        href "/counter"
+                        "Counter"
+                    }
+                }
+            |]
+        }
+    }
+
+let routes = html.route [| routeCi "/counter" counterPage; routeAny homePage |]
+
+
+let app =
+    ErrorBoundary'() {
+#if DEBUG
+        key (System.Random.Shared.Next()) // So hot reload can re-render correctly
+#endif
+        ErrorContent(fun e ->
+            Alert'() {
+                Type AlertType.Error
+                string e
+            }
+        )
+        AntContainer'.create ()
+        Layout'() {
+            style { height "100%" }
+            childContent [|
+                appbar
+                Layout'.create [|
+                    navmenus
+                    Layout'() {
+                        style { padding 10 }
+                        Content'() { routes }
+                    }
+                |]
+            |]
+        }
     }
--- samples/FunWasm.AntDesignDemo/Startup.fs
+++ samples/FunWasm.AntDesignDemo/Startup.fs
@@ -3,17 +3,19 @@
 open System
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
+open AntDesign
 open Fun.Blazor
 open FunWasm.AntDesignDemo
 
 let builder = WebAssemblyHostBuilder.CreateDefault(Environment.GetCommandLineArgs())
 
 #if DEBUG
-builder.AddFunBlazor("#app", html.hotReloadComp(app, "FunWasm.AntDesignDemo.App.app"))
+builder.AddFunBlazor("#app", html.hotReloadComp (app, "FunWasm.AntDesignDemo.App.app"))
 #else
 builder.AddFunBlazor("#app", app)
 #endif
 
 builder.Services.AddFunBlazorWasm()
+builder.Services.AddAntDesign()
 
 builder.Build().RunAsync()
